b7388b3787413427b7552aba0691ed35b06373a9
SheisAlexander <407631422@gms.tku.edu.tw>
2021-07-09 04:12:38 +0800

    text box shape big

---
+++ app/src/main/java/com/example/english/Collection_article.java
@@ -0,0 +1,14 @@
+package com.example.english;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Collection_article extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collection_article);
+    }
+}
