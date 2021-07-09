5c5d97081a07e19da9e58a2616ac8c701ef77522
ImWinnieLiao <407631125@gms.tku.edu.tw>
2021-07-09 13:07:05 +0800

    seekbar程式碼設定

--- app/src/main/java/com/example/english/YourLevel.java
+++ app/src/main/java/com/example/english/YourLevel.java
@@ -10,5 +10,30 @@ public class YourLevel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_your_level);
+
+        mSeekBar = (SeekBar) findViewById(R.id.seekBar);
+        mSeekBar.setOnSeekBarChangeListener(seekBarOnSeekBarChange);
     }
+
+    private SeekBar.OnSeekBarChangeListener seekBarOnSeekBarChange
+            = new SeekBar.OnSeekBarChangeListener()
+    {
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar)
+        {
+            //停止拖曳時觸發事件
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar)
+        {
+            //開始拖曳時觸發事件
+        }
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+        {
+            //拖曳途中觸發事件，回傳參數 progress 告知目前拖曳數值
+        }
+    };
 }
