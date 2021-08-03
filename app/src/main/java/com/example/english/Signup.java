141da2480c462fc78197cf6fd96700141ec8d38a
ImWinnieLiao <407631125@gms.tku.edu.tw>
2021-08-03 18:50:06 +0800

    連線firebase

--- app/src/main/java/com/example/english/Signup.java
+++ app/src/main/java/com/example/english/Signup.java
@@ -3,12 +3,22 @@ package com.example.english;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Signup extends AppCompatActivity {
+
+    EditText username, password,email;
+    Button SignupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        username = findViewById(R.id.editTextEmail);
+        password = findViewById(R.id.editTextPassword);
+        email = findViewById(R.id.editTextUserName);
+        SignupBtn = findViewById(R.id.Signupbutton);
     }
 }
