6736a25445f5756eb9947bb14dab46065b6d3753
ImWinnieLiao <407631125@gms.tku.edu.tw>
2021-08-11 23:06:13 +0800

    Test

--- app/src/main/java/com/example/english/Signup.java
+++ app/src/main/java/com/example/english/Signup.java
@@ -16,7 +16,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.auth.User;
+
 
 
 public class Signup extends AppCompatActivity implements View.OnClickListener{
