8a929f8a0549697ac05b7760823dfea3f5940f59
1ruuu <irene45666@gmail.com>
2021-08-17 21:18:23 +0800

    ♥

--- app/src/main/java/com/example/english/Settings.java
+++ app/src/main/java/com/example/english/Settings.java
@@ -1,6 +1,7 @@
 package com.example.english;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,11 +11,20 @@ import android.widget.Button;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Settings extends AppCompatActivity {
-
+    private CardView logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        logout = (CardView) findViewById(R.id.signOut);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(Settings.this,Login.class));
+            }
+        });
     }
 }
