6411aac50276cdd73113d7e0b7c1b355ad2ca604
SheisAlexander <407631422@gms.tku.edu.tw>
2021-08-27 02:41:02 +0800

    8/27 字典介面改好

--- app/src/main/java/com/example/english/article.java
+++ app/src/main/java/com/example/english/article.java
@@ -2,7 +2,6 @@ package com.example.english;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
@@ -10,8 +9,6 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 import java.util.Locale;
 
