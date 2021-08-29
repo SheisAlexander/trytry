cd8938cc56446c60c0188b67915cf61ef2844e22
ImWinnieLiao <407631125@gms.tku.edu.tw>
2021-08-29 16:36:34 +0800

    有聲字典

--- app/src/main/java/com/example/english/Dictionary.java
+++ app/src/main/java/com/example/english/Dictionary.java
@@ -1,32 +1,43 @@
 package com.example.english;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.gson.JsonParser;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.util.Locale;
+
 public class Dictionary extends AppCompatActivity {
 
     Button dicButton;
+    ImageButton soundButton;
     EditText wordText;
     TextView dicText;
 
@@ -35,10 +46,8 @@ public class Dictionary extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
 
-
         //Initialize and Assign Variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-
         //Set home selected
         bottomNavigationView.setSelectedItemId(R.id.dictionary);
         //Perform ItemSelectedListener
@@ -65,9 +74,10 @@ public class Dictionary extends AppCompatActivity {
             }
         });
 
-        dicButton = findViewById(R.id.DictionaryButton);
-        wordText = findViewById(R.id.WordText);
-        dicText = findViewById(R.id.DictionaryText);
+        dicButton   = findViewById(R.id.DictionaryButton);
+        wordText    = findViewById(R.id.WordText);
+        dicText     = findViewById(R.id.DictionaryText);
+        soundButton = findViewById(R.id.soundButton);
 
         dicButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -100,8 +110,6 @@ public class Dictionary extends AppCompatActivity {
                             JSONObject worddefs = wordmean.getJSONObject(0);
                             worddef = worddefs.getString("definition");
 
-
-
                             //worddef = wordInfo.getString("meanings");
 
 
@@ -110,7 +118,7 @@ public class Dictionary extends AppCompatActivity {
                         }
 
                         dicText.append("詞性: "+"\n"+wordpos+"意思: "+"\n"+worddef);
-                        //dicText.setText("Part of Speech: "+wordpos+"\n" +"Mean:"+worddef+"\n");
+
                         //Toast.makeText(Dictionary.this, "Mean: "+worddef, Toast.LENGTH_SHORT).show();
                     }
                 }, new Response.ErrorListener() {
@@ -120,6 +128,7 @@ public class Dictionary extends AppCompatActivity {
                     }
                 });
                 queue.add(request);
+
                 // Request a string response from the provided URL.
 //                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
 //                        new Response.Listener<String>() {
@@ -138,6 +147,61 @@ public class Dictionary extends AppCompatActivity {
 
 
                 //Toast.makeText(Dictionary.this, ":)", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        soundButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playAudio();
+            }
+
+            private void playAudio() {
+                final TextView textView = (TextView) findViewById(R.id.text);
+
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(Dictionary.this);
+                String url ="https://api.dictionaryapi.dev/api/v2/entries/en/" + wordText.getText().toString();
+                // initializing media player
+                MediaPlayer mediaPlayer = new MediaPlayer();
+
+                // below line is use to set the audio
+                // stream type for our media player.
+                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+                JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET,url,null,new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        String wordsound = "";
+                        try {
+                            //第一層
+                            JSONObject wordInfo = response.getJSONObject(0);
+                            JSONArray phonetics = wordInfo.getJSONArray("phonetics");
+                            //第二層
+                            JSONObject audio = phonetics.getJSONObject(0);
+                            wordsound = audio.getString("audio");
+
+                            String url1 = "https:"+wordsound;
+
+                            mediaPlayer.setDataSource(url1);
+                            mediaPlayer.prepare();
+                            mediaPlayer.start();
+
+
+                        } catch (JSONException | IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        //Toast.makeText(Dictionary.this, "Mean: "+worddef, Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Dictionary.this, "Wrong", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                queue.add(request);
+
             }
         });
     }
